cb1a899deae82a4fb66d46cd620a1dbc0ad0bcbd
Kiran Sharma <kiran.sharma@gurzu.com>
2022-05-12 15:56:29 +0545

    UI implemented

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? =null
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
+
     }
+
+
 }
